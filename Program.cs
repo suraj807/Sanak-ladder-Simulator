01f53e228513bf0eeea59321dffe5e46d08e2d25
suraj807 <surajdev805@gmail.com>
2022-03-25 21:22:13 +0530

    player rolls die random number

--- Program.cs
+++ Program.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace Snake_and_Ladder_Problem
 {
-    // master
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-          
-            Console.WriteLine("Welcome to Snake Ladder Game");
-            Console.WriteLine("Let's Play");
-            Console.WriteLine("Currently you are at 0 Position");
+
+            int dicenum;
+
+            Random random = new Random();
+            dicenum = random.Next(1, 7);
+            Console.WriteLine($"After rolling Dice,The number display on the Dice is : {dicenum}");
 
             Console.ReadLine();
         }
