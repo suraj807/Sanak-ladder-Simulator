1c61cd5e79d5a57ece461cd30a7d37e6fa9db19a
suraj807 <surajdev805@gmail.com>
2022-03-25 19:14:13 +0530

    Snake and ladder problem

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Welcome to Snake Ladder Game");
+           
+         Console.ReadLine();
+        }
+    }
+}
